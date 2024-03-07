183cf62aaaca802a72442925aa3d41648b5635cc
Nalini Prasad Dash <nalinidash@protonmail.ch>
2024-03-07 20:16:26 +0530

    Complete etch-a-sketch project

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
 
-// Adding 256 squares into the container
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("square");
-  // div.setAttribute("id", `sqr-${i}`);
-  // console.log(div.getAttribute("id")); // testing purpose
-  container.appendChild(div);
-  div.addEventListener("mouseenter", () => {
-    div.classList.add("hovered");
-  });
-}
+let numOfSqrsPerSide = 0;
+
+button.addEventListener("click", () => {
+  // Set limit for number(positive and less than 100)
+  do {
+    numOfSqrsPerSide = Number(
+      window.prompt(
+        "Number of squares per side for the new grid(Limit: 100)",
+        "10"
+      )
+    );
+  } while (numOfSqrsPerSide > 100 || numOfSqrsPerSide < 0);
+
+  // Remove the existing grids
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+
+  // Adding required nos of etch-a-sketch squares into the container
+  for (let i = 0; i < Math.pow(numOfSqrsPerSide, 2); i++) {
+    const div = document.createElement("div");
+    div.style.width = `${960 / numOfSqrsPerSide}px`;
+    div.classList.add("square");
+    container.appendChild(div);
+    div.addEventListener("mouseenter", () => {
+      div.classList.add("hovered");
+    });
+  }
+});
